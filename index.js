b55594784f0a1b3714a02e6d11fdea732998de07
el-sam <samurai.h@yahoo.com>
2023-11-17 14:17:04 +0800

    many changes

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
 // Add your code here
-function submitData(){
+// function submitData(name,email) {
     
+    
+//     const obj = {
+//         method: "POST",
+//         Headers: {
+//             "Content-Type" : "Application/json",
+//             "Accept":"Application/json",
+
+//         },
+//         body: JSON.stringify({
+//             name:name,
+//             email:email,
+
+//         })
+//     }
+//     fetch("http://localhost:3000/users", obj)
+//         .then(res => res.json())
+//         .then(data => console.log(data))
+//         .catch(error => {
+//             alert("eorror happened")
+//             console.log(error.mesaage)
+//         })
+
+
+// }
+
+// submitData("aa","bb")
+
+const form = document.querySelector("form")
+form.addEventListener('submit', submitForm)
+
+function submitForm(event) {
+    event.preventDefault();
+    console.log(document.querySelector("#name").ariaValueMin)
+    console.log(form.querySelector("#name").value)
+    console.log(event.target.name.value)
+    console.log(form.name.value)
+    
+    console.log()
 }
+
